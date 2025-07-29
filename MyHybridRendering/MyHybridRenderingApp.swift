5af3fef29401e33da9925cd9c5f30aaeac709cd0
Caroline <c.b100@aol.com>
2025-07-30 09:12:42 +1000

    Initial Commit

---
+++ MyHybridRendering/MyHybridRenderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHybridRenderingApp.swift
+//  MyHybridRendering
+//
+//  Created by Caroline on 30/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyHybridRenderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
